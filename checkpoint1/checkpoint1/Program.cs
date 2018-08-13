0a91536689dafa0845b09594c4b7607d87fc3315
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-08-13 14:52:02 +0200

    s

---
+++ checkpoint1/checkpoint1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace checkpoint1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  string[] NameList;
+
+            Console.WriteLine("Ange kommando: ");
+            string response = Console.ReadLine();
+
+            string[] list = response.Split("-");
+            // NameList = NameArray(response);
+
+
+            foreach (var item in list)
+            {
+               
+                int tal = int.Parse(item);
+                //int rows = 1;
+                string str = "";
+                for (int j = 0; j < tal; j++)
+                {
+                    Console.WriteLine(str += "*");
+                    //    Console.WriteLine();
+                    //    for (int i = 0; i < rows; i++)
+                    //    {
+
+                    //        Console.Write("*");
+                    //    }
+                    //rows++;
+                    //Console.WriteLine();
+                }
+            }
+
+        }
+
+        //static string[] NameArray(string response)
+        //{
+        //    string[] list = response.Split("-");
+        //    return list;
+        //}
+    }
+}
