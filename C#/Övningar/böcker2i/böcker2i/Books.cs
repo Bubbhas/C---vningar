6f4eed220241dd00808541efa61842309b9c92cf
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-08-14 11:21:43 +0200

    Andra bokuppgiften typ klar

---
+++ C#/Övningar/böcker2i/böcker2i/Books.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace böcker2i
+{
+   class Produkter
+    {
+        int ProductId;
+
+        public void SetProductId(int v)
+        {
+            ProductId = v;
+        }
+        public int GetProductId()
+        {
+            return ProductId;
+        }
+
+    }
+    class ElectronicBook : Books
+    {
+        public void SendBookTo(string emailAdress)
+        {
+            Console.WriteLine("Boken skickas till" + emailAdress);
+        }
+    }
+    class Books : Produkter
+    {
+      
+
+        string Isbn;
+        string Author;
+        int Pages;
+
+        public void SetIsbn(string v)
+        {
+            Isbn = v;
+        }
+        public string GetIsbn()
+        {
+            return Isbn;
+        }
+        public void SetAuthor(string v)
+        {
+            Author = v;
+        }
+        public string GetAuthor()
+        {
+            return Author;
+        }
+        public void SetPages(int value)
+        {
+            if (value > 0 && value < 1000)
+            {
+                Pages = value;
+            }
+            else
+            {
+                Pages = 300;
+            }
+        }
+        public int GetPages()
+        {
+            return Pages;
+        }
+        public double WeightInGram()
+        {
+            double Weight = Pages * 0.8;
+            return Weight;
+        }
+        public string SetSize()
+        {
+            if (Pages < 100)
+            {
+
+                return "Tunn";
+            }
+            else if (Pages < 299 && Pages > 100)
+            {
+
+                return "Normal";
+            }
+            else
+            {
+
+                return "Tjock";
+            }
+
+        }
+    }
+}
---
+++ C#/Övningar/böcker2i/böcker2i/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace böcker2i
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Books();
+
+            b1.SetIsbn("978-3-16-148410-0");
+            b1.SetAuthor("Kalle");
+            b1.SetPages(400);
+            b1.SetProductId(1234);
+
+            var e1 = new ElectronicBook();
+            e1.SetProductId(5678);
+
+            
+            bool isBook = b1 is Books;
+            bool isEbook = b1 is ElectronicBook;
+            bool isProduct = b1 is Produkter;
+
+            bool isBooke = e1 is Books;
+            bool isEbooke = e1 is ElectronicBook;
+            bool isProducte = e1 is Produkter;
+
+
+            Console.WriteLine($"Info om boken:");
+            Console.WriteLine();
+            Console.WriteLine($"   ISBN:          {b1.GetIsbn()}");
+            Console.WriteLine($"   Författare:    {b1.GetAuthor()}");
+            Console.WriteLine($"   Antal sidor:   {b1.GetPages()}");
+            Console.WriteLine($"   Vikt:          {b1.WeightInGram()} gram");
+            Console.WriteLine($"   Storlek:       {b1.SetSize()}");
+            Console.WriteLine($"   Produktens Id: {b1.GetProductId()}");
+            Console.WriteLine();
+            Console.WriteLine("Är b1 en bok?     " + isBook);
+            Console.WriteLine("Är b1 en ebok?    " + isEbook);
+            Console.WriteLine("Är b1 en bok?     " + isProduct);
+            Console.WriteLine();
+            Console.WriteLine("Är e1 en bok?     " + isBooke);
+            Console.WriteLine("Är e1 en ebok?    " + isEbooke);
+            Console.WriteLine("Är e1 en bok?     " + isProducte);
+            Console.WriteLine();
+
+
+            e1.SendBookTo(" jesper");
+        }
+    }
+}
