cf4a94bbeaa33c9daa5b51ad60ce7c4d3480f5ba
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-08-14 10:37:42 +0200

    böcker första klar

---
+++ C#/Övningar/Böcker1i/Böcker1i/Book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Böcker1i
+{
+    public class Book
+    {
+
+        public string Isbn { get; set; }
+        public string Name { get; set; }
+        public int Pages { get; set; }
+        public double Weight { get; set; }
+        
+      
+
+        public void SetIsbn(string v)
+        {
+            Isbn = v;
+        }
+        public void SetName(string v)
+        {
+            Name = v;
+        }
+
+        public void SetPages(int v)
+        {
+            Pages = v;
+        }
+
+        public double WeightInGram()
+        {
+            Weight = Pages * 0.8;
+            return Weight;
+        }
+        public string SetSize()
+        {
+            if (Pages < 100)
+            {
+               
+                return "Tunn";
+            }
+            else if (Pages < 299 && Pages > 100)
+            {
+                
+                return "Normal";
+            }
+            else
+            {
+                
+                return "Tjock";
+            }
+
+        }
+
+    }
+
+
+
+}
---
+++ C#/Övningar/Böcker1i/Böcker1i/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Böcker1i
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Book();
+            
+            b1.SetIsbn("978-3-16-148410-0");
+            b1.SetName("Kalle");
+            b1.SetPages(400);
+
+            Console.WriteLine($"Info om boken:");
+            Console.WriteLine();
+            Console.WriteLine($"   ISBN:        {b1.Isbn}");
+            Console.WriteLine($"   Författare:  {b1.Name}");
+            Console.WriteLine($"   Antal sidor: {b1.Pages}");
+            Console.WriteLine($"   Vikt:        {b1.WeightInGram()} gram");
+            Console.WriteLine($"   Storlek:     {b1.SetSize()}");
+            Console.WriteLine();
+        }
+    }
+}
