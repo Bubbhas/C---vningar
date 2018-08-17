1b31309851837475ddda513318408ab241da160f
Jesper Eriksson <jesper--eriksson@hotmail.com>
2018-08-17 16:06:27 +0200

    innan avslut vecka 2

---
+++ C#/Extrauppgifter/Override ToString 6i3i/Override ToString 6i3i/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Override_ToString_6i3i
+{
+    class Program
+    {
+
+        class Circle
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int radius { get; set; }
+
+        }
+        class Rectangle
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+        class Trinagle
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            var c = new Circle();
+            var r = new Rectangle();
+            var t = new Trinagle();
+        }
+    }
+}
